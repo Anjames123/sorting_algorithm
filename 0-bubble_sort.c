0ad9944e1f038a4ab44ad2661e996fcab3baceb4
Anjames123 <ajibadejames19@gmail.com>
2023-04-24 03:27:07 -0700

    Bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -32,3 +32,16 @@ void bubble_sort(int *array, size_t size)
 			break;
 	}
 }
+
+/**
+ * swap - swaps two elements
+ * @a: first element
+ * @b: second element
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
