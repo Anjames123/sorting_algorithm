d9b1f8aad8c07d4d34d8b3effeb918fbf28744cf
Amypetua <anachusichiamaka32@gmail.com>
2023-04-25 05:01:54 -0700

    Selection sort

--- sort.h
+++ sort.h
@@ -20,11 +20,15 @@ typedef struct listint_s
 	struct listint_s *next;
 
 } listint_t;
-/* Printing function */
+/* Printing functions */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
-/* Sorting Algorithm */
+/* Helper functions */
+void swap(int *a, int *b);
+int len_list(listint_t *h);
+
+/* Sorting Algorithm functions */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -37,8 +41,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void swap(int *i, int *j);
-int len_list(listint_t *h);
 
 
 #endif /* SORT_H */
