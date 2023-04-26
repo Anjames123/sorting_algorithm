7ccc9d8ad5decc5715d8c444d9a0f7cb45b02e39
Anjames123 <ajibadejames19@gmail.com>
2023-04-25 18:10:36 -0700

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,7 +3,7 @@
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers in ascending
  *                         order using the Insertion sort algorithm
- *                         @list: A pointer to the head of the doubly linked list
+ * @list: A pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
